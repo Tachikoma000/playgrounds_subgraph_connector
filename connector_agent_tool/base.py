ea55992d55d2ab1c33688d2e741622f698b472a4
Tachi <89101179+Tachikoma000@users.noreply.github.com>
2023-10-12 03:45:29 -0500

    Title: Enhance PlaygroundsSubgraphConnector for Better Pagination & Error Handling
    - Refactored the PlaygroundsSubgraphConnectorToolSpec class for improved readability and maintainability.
    - Implemented a safety mechanism to prevent infinite loops during pagination.
    - Added logic to detect and handle identical data received in consecutive queries.
    - Improved error handling for various types of request failures.
    - Updated class and method docstrings for clarity and detailed explanations.

--- connector_agent_tool/base.py
+++ connector_agent_tool/base.py
@@ -7,32 +7,32 @@ from llama_hub.tools.graphql.base import GraphQLToolSpec
 
 class PlaygroundsSubgraphConnectorToolSpec(GraphQLToolSpec):
     """
-    Connects to subgraphs on The Graph's decentralized network via the Playgrounds API.
+    A tool specification that provides connectivity to subgraphs on The Graph's decentralized network via the Playgrounds API.
+    This tool facilitates GraphQL queries to the specified subgraphs and supports pagination.
     
     Attributes:
-        spec_functions (list): List of functions that specify the tool's capabilities.
-        url (str): The endpoint URL for the GraphQL requests.
-        headers (dict): Headers used for the GraphQL requests.
+        spec_functions (list of str): The list of functions that this tool provides.
+        url (str): The endpoint URL to which GraphQL requests are sent.
+        headers (dict): Any necessary headers for the GraphQL requests, such as authentication.
     """
 
     spec_functions = ["graphql_request"]
 
-    def __init__(self, identifier: str, api_key: str, use_deployment_id: bool = False, log_level: int = logging.INFO):
+    def __init__(self, identifier: str, api_key: str, use_deployment_id: bool = False, log_level: int = logging.INFO, paginate: bool = True):
         """
-        Initialize the connector.
-        
+        Initialize the PlaygroundsSubgraphConnector with necessary parameters.
+
         Args:
-            identifier (str): Subgraph identifier or Deployment ID.
-            api_key (str): API key for the Playgrounds API.
-            use_deployment_id (bool): Flag to indicate if the identifier is a deployment ID. Default is False.
-            log_level (int, optional): Logging level. Use constants from Python's logging module (e.g., logging.DEBUG, logging.INFO). Default is logging.INFO.
-        
-        Note:
-            To utilize the logging, set up a basic configuration at the entry point of your application using `logging.basicConfig(level=logging.DEBUG)` or any desired log level.
+            identifier (str): Unique identifier for the subgraph or deployment.
+            api_key (str): API key to authenticate with the Playgrounds API.
+            use_deployment_id (bool, optional): Whether the provided identifier is a deployment ID. Defaults to False.
+            log_level (int, optional): Desired log level for the connector. Defaults to logging.INFO.
+            paginate (bool, optional): Whether to paginate the results if possible. Defaults to True.
         """
         
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
+        self.paginate = paginate
         
         endpoint = "deployments" if use_deployment_id else "subgraphs"
         self.url = f"https://api.playgrounds.network/v1/proxy/{endpoint}/id/{identifier}"
@@ -43,50 +43,80 @@ class PlaygroundsSubgraphConnectorToolSpec(GraphQLToolSpec):
 
     def graphql_request(self, query: str, variables: Optional[dict] = None, operation_name: Optional[str] = None) -> Union[dict, str]:
         """
-        Make a GraphQL query.
+        Execute a GraphQL query against the connected subgraph.
 
         Args:
-            query (str): The GraphQL query string to execute.
-            variables (dict, optional): Variables for the GraphQL query. Default is None.
-            operation_name (str, optional): Name of the operation, if multiple operations are present in the query. Default is None.
+            query (str): The GraphQL query string.
+            variables (dict, optional): Variables to be used in the GraphQL query. Defaults to None.
+            operation_name (str, optional): The name of the operation if the query string contains multiple operations. Defaults to None.
 
         Returns:
-            dict: The response from the GraphQL server if successful.
-            str: Error message if the request fails.
+            dict: The response from the GraphQL server if the query is successful.
+            str: An error message if the query fails.
         """
+        
+        all_data = []
+        last_id = ""
+        loop_counter = 0
+        previous_data = None
 
-        payload = {"query": query.strip()}
-        if variables:
-            payload["variables"] = variables
-        if operation_name:
-            payload["operationName"] = operation_name
+        # Loop to handle pagination
+        while True:
+            loop_counter += 1
+            if loop_counter > 10:  # safety mechanism to prevent infinite loops
+                self.logger.warning("Exiting loop after 10 iterations.")
+                break
 
-        self.logger.info(f"Sending GraphQL request to {self.url} with query: {query}")
+            # Prepare the request payload
+            payload = {"query": query.strip()}
+            if variables:
+                payload["variables"] = dict(variables)
+                payload["variables"]["id_gt"] = last_id
+            if operation_name:
+                payload["operationName"] = operation_name
 
-        try:
-            response = requests.post(self.url, headers=self.headers, json=payload)
-            response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+            self.logger.info(f"Sending GraphQL request to {self.url}")
+            
+            # Handle possible exceptions during the request
+            try:
+                response = requests.post(self.url, headers=self.headers, json=payload)
+                response.raise_for_status()
+                data = response.json()
+                self.logger.debug(f"Received data: {data}")
 
-            data = response.json()
-            if 'errors' in data:
-                self.logger.error(f"GraphQL errors: {data['errors']}")
-                return data['errors']
+                # If same data is received in consecutive queries, break out of the loop
+                if previous_data == data:
+                    self.logger.warning("Identical data received in consecutive queries. Exiting loop.")
+                    break
+                previous_data = data
+                
+                if 'errors' in data:
+                    self.logger.error(f"GraphQL errors: {data['errors']}")
+                    return data['errors']
 
-            self.logger.info(f"Successfully retrieved data from {self.url}")
-            return data
+                # If pagination is not needed or data isn't present, return the data
+                if not self.paginate or not data.get("data"):
+                    return data
 
-        except requests.ConnectionError:
-            self.logger.error("Failed to connect to the server.")
-            return "Connection Error"
+                # If data is present and of type list, continue processing
+                if 'data' in data and isinstance(data["data"], list) and data["data"]:
+                    all_data.extend(data["data"])
+                    last_id = data["data"][-1]["id"]
+                    self.logger.debug(f"Last ID processed: {last_id}")
+                    if len(data["data"]) < 1000:  # 1000 is a common page size in GraphQL
+                        break
 
-        except requests.Timeout:
-            self.logger.error("Request timed out.")
-            return "Timeout Error"
+            except requests.ConnectionError:
+                self.logger.error("Failed to connect to the server.")
+                return "Connection Error"
+            except requests.Timeout:
+                self.logger.error("Request timed out.")
+                return "Timeout Error"
+            except requests.RequestException as e:
+                self.logger.error(f"Error during request: {e}")
+                return str(e)
+            except ValueError as e:
+                self.logger.error(f"JSON decoding error: {e}")
+                return f"Decoding Error: {e}"
 
-        except requests.RequestException as e:
-            self.logger.error(f"Error occurred while making the request: {e}")
-            return str(e)
-
-        except ValueError as e:
-            self.logger.error(f"Error decoding JSON: {e}")
-            return f"Error decoding JSON: {e}"
+        return {"data": all_data}
--- connector_agent_tool/test.py
+++ connector_agent_tool/test.py
@@ -14,14 +14,15 @@ def simple_test():
     Run a simple test querying the financialsDailySnapshots from Uniswap V3 subgraph using OpenAIAgent and Playgrounds API.
     """
     # Set the OpenAI API key
-    openai.api_key = 'YOUR_OPENAI_API_KEY'
+    openai.api_key = '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'
     
     # Initialize the tool specification
     connector_spec = PlaygroundsSubgraphConnectorToolSpec(
-        identifier="YOUR_SUBGRAPH_OR_DEPLOYMENT_IDENTIFIER", 
-        api_key="YOUR_PLAYGROUNDS_API_KEY", 
+        identifier="ELUcwgpm14LKPLrBRuVvPvNKHQ9HvwmtKgKSH6123cr7", 
+        api_key="pg-AqAFS8G3TN3Kagdgw2MrGjFvDGUgxImS", 
         use_deployment_id=False,
-        log_level=logging.DEBUG
+        log_level=logging.DEBUG,
+        paginate=False
     )
     
     # Setup agent with the tool
@@ -29,10 +30,17 @@ def simple_test():
     
     # Make a query using the agent
     response = agent.chat(
-        'query the financialsDailySnapshots for id, timestamp, totalValueLockedUSD, and dailyVolumeUSD. only give me the first 2 rows'
+        """ 
+        query($last_id: ID) {
+            financialsDailySnapshots(first: 2) {
+                id
+                timestamp
+                totalValueLockedUSD
+            }
+            }
+        """
     )
     print(response)
 
 if __name__ == "__main__":
     simple_test()
-
